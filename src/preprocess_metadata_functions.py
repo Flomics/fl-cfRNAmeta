e50dc387c93bd51a06dfbf08ac44dc633ebed66a
Cristina Tuñí <cristina.tuni@flomics.com>
2025-05-13 14:20:57 +0200

    changes in metadata creation

---
+++ src/preprocess_metadata_functions.py
@@ -0,0 +1,351 @@
+import pandas as pd
+import numpy as np
+
+def preprocess_chen():
+    csv_path = "../sra_metadata/chen_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "chen"
+    df["dataset_batch"] = "chen"
+    #df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    #df["plasma_tubes"] = "EDTA-coated vacutainer"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit"
+    df["rna_extraction_kit_short"]="Norgen"
+    #df["dnase"]="DNase I"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/chen_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_zhu():
+    csv_path = "../sra_metadata/zhu_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "zhu"
+    df["dataset_batch"] = "zhu"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "Unspecified"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit"
+    df["rna_extraction_kit_short"]="Norgen"
+    df["dnase"]="DNase I"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/zhu_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_roskams():
+    csv_path = "../sra_metadata/roskams_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "roskams"
+    df["dataset_batch"] = np.where(df["Cohort"] == "pilot", "rokams_1", "roskams_2")
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA-anticoagulated vacutainers"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit"
+    df["rna_extraction_kit_short"]="Norgen"
+    df["dnase"]="Baseline-ZERO"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/roskams_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_ngo():
+    csv_path = "../sra_metadata/ngo_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "ngo"
+    df["dataset_batch"] = "ngo"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit"
+    df["rna_extraction_kit_short"]="Norgen"
+    df["dnase"]="Baseline-ZERO"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/ngo_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_ibarra():
+    csv_path = "../sra_metadata/ibarra_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "ibarra"
+    df["dataset_batch"] = "ibarra"
+    df["biomaterial"] = df["tissue"].str.lower()
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-exome capture"
+    df["plasma_tubes"] = df["biomaterial"].apply(lambda x: "EDTA tubes" if x == "plasma" else "BD Vacutainer clotting tubes" if x == "serum" else "")
+    df["rna_extraction_kit"]="Qiagen QIAamp Circulating Nucleic Acid Kit"
+    df["rna_extraction_kit_short"]="Qiagen QIAamp"
+    df["dnase"]="Turbo DNAse"
+    df["library_prep_kit"]="Unspecified"
+    df["library_prep_kit_short"]="Unspecified"
+    df.to_csv("../sra_metadata/ibarra_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_toden():
+    csv_path = "../sra_metadata/toden_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "toden"
+    df["dataset_batch"] = "toden"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-exome capture"
+    df["plasma_tubes"] = "Unspecified"
+    df["rna_extraction_kit"]="Qiagen QIAamp Circulating Nucleic Acid Kit"
+    df["rna_extraction_kit_short"]="Qiagen QIAamp"
+    df["dnase"]="No"
+    df["library_prep_kit"]="Swift 2S kit"
+    df["library_prep_kit_short"]="Swift 2S kit"
+    df.to_csv("../sra_metadata/toden_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_chalasani():
+    csv_path = "../sra_metadata/chalasani_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "chalasani"
+    df["dataset_batch"] = "chalasani"
+    df["biomaterial"] = "blood serum"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-exome capture"
+    df["plasma_tubes"] = "BD Vacutainer clotting tubes"
+    df["rna_extraction_kit"]="Qiagen QIAamp Circulating Nucleic Acid Kit"
+    df["rna_extraction_kit_short"]="Qiagen QIAamp"
+    df["dnase"]="Turbo DNAse"
+    df["library_prep_kit"]="Unspecified"
+    df["library_prep_kit_short"]="Unspecified"
+    df.to_csv("../sra_metadata/chalasani_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_block():
+    csv_path = "../sra_metadata/block_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "block"
+    df["dataset_batch"] = np.where(
+        abs(df["AvgSpotLen"] - 150) < abs(df["AvgSpotLen"] - 300),
+        "block_1", "block_2"
+    )
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "Unspecified"
+    df["rna_extraction_kit"]="Qiagen RNeasy Serum/Plasma Kit"
+    df["rna_extraction_kit_short"]="Qiagen RNeasy Plasma"
+    df["dnase"]="No"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/block_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_rozowsky(): #this metadata does not come from SRA 
+    csv_path = "../sra_metadata/rozowsky_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "rozowsky"
+    df["dataset_batch"] = "rozowsky"
+    df["biomaterial"] = "tissue"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "NA"
+    df["rna_extraction_kit"]="Qiagen RNeasy Serum/Plasma Kit"
+    df["rna_extraction_kit_short"]="Qiagen RNeasy Plasma"
+    df["dnase"]="DNAse I"
+    df["library_prep_kit"]="custom"
+    df["library_prep_kit_short"]="custom"
+    df.to_csv("../sra_metadata/rozowsky_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_tao():
+    csv_path = "../sra_metadata/tao_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "tao"
+    df["dataset_batch"] = "tao"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit"
+    df["rna_extraction_kit_short"]="Norgen"
+    df["dnase"]="DNAse I"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/tao_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_wei():
+    csv_path = "../sra_metadata/wei_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "wei"
+    df["dataset_batch"] = "wei"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "DNA"
+    df["library_selection"] = "whole-genome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="NA"
+    df["rna_extraction_kit_short"]="NA"
+    df["dnase"]="No"
+    df["library_prep_kit"]="Truseq Nano DNA HT"
+    df["library_prep_kit_short"]="Truseq Nano DNA HT"
+    df.to_csv("../sra_metadata/wei_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_moufarrej(): # we need to find a way to separate this dataset into the "sites" and into the cohorts. also decide if we want to merge by replicate
+    csv_path = "../sra_metadata/moufarrej_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "moufarrej"
+    df["dataset_batch"] = "moufarrej"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="Norgen Plasma/Serum Circulating RNA and Exosomal Purification kit Slurry"
+    df["rna_extraction_kit_short"]="Norgen (slurry)"
+    df["dnase"]="Baseline-ZERO"
+    df["library_prep_kit"]="SMARTer Stranded Pico v2"
+    df["library_prep_kit_short"]="SMARTer Pico v2"
+    df.to_csv("../sra_metadata/moufarrej_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_wang():
+    csv_path = "../sra_metadata/wang_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "wang"
+    df["dataset_batch"] = "wang"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="Apostle MiniMaxTM High-Efficiency cfRNA Isolation Kit"
+    df["rna_extraction_kit_short"]="Apostle"
+    df["dnase"]="No"
+    df["library_prep_kit"]="Own protocol"
+    df["library_prep_kit_short"]="Own protocol"
+    df.to_csv("../sra_metadata/wang_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_giraldez():
+    csv_path = "../sra_metadata/giraldez_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "giraldez"
+    df["dataset_batch"] = np.where(
+        abs(df["AvgSpotLen"] - 25) < abs(df["AvgSpotLen"] - 50),
+        "giraldez_1", "giraldez_2"
+    )
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="Qiagen miRNeasy Mini Kit"
+    df["rna_extraction_kit_short"]="Qiagen miRNeasy"
+    df["dnase"]="No"
+    df["library_prep_kit"]="Illumina TruSeq small RNA"
+    df["library_prep_kit_short"]="Illumina TruSeq small RNA"
+    df.to_csv("../sra_metadata/giraldez_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_sun():
+    csv_path = "../sra_metadata/giraldez_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "sun"
+    df["biomaterial"] = df["tissue"].apply(lambda x: "blood plasma" if x == "plasma" else "blood serum" if x == "serum" else "")
+    df["dataset_batch"] = np.where(df["biomaterial"] == "blood plasma", "sun_1", "sun_2")
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "EDTA"
+    df["rna_extraction_kit"]="MOF method"
+    df["rna_extraction_kit_short"]="MOF"
+    df["dnase"]="DNAse I"
+    df["library_prep_kit"]="SMARTer Stranded Total RNA-Seq"
+    df["library_prep_kit_short"]="SMARTer Total"
+    df.to_csv("../sra_metadata/sun_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_decruyenaere():
+    csv_path = "../sra_metadata/decruyenaere_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "decruyenaere"
+    df["dataset_batch"] = "decruyenaere"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "PAXgene blood ccfDNA"
+    df["rna_extraction_kit"]="Qiagen miRNeasy Serum/Plasma kit"
+    df["rna_extraction_kit_short"]="Qiagen miRNeasy Plasma"
+    df["dnase"]="HL-dsDNase"
+    df["library_prep_kit"]="SMARTer stranded Pico v3"
+    df["library_prep_kit_short"]="SMARTer Pico v3"
+    df.to_csv("../sra_metadata/decruyenaere_metadata_preprocessed.csv", index=False)
+
+    return df
+
+def preprocess_reggiardo():
+    csv_path = "../sra_metadata/decruyenaere_metadata.csv"
+    df = pd.read_csv(csv_path)
+
+    df["dataset_short_name"] = "reggiardo"
+    df["dataset_batch"] = "reggiardo"
+    df["biomaterial"] = "blood plasma"
+    df["nucleic_acid_type"] = "total RNA"
+    df["library_selection"] = "whole-transcriptome"
+    df["plasma_tubes"] = "K2EDTA"
+    df["rna_extraction_kit"]="Qiagen ExoRNeasy kit"
+    df["rna_extraction_kit_short"]="Qiagen ExoRNeasy"
+    df["dnase"]="No"
+    df["library_prep_kit"]="Takara SMART-Seq HT kit+Illumina Nextera XT DNA Prep "
+    df["library_prep_kit_short"]="SMART-Seq"
+    df.to_csv("../sra_metadata/reggiardo_metadata_preprocessed.csv", index=False)
+
+    return df
+
+
+if __name__ == "__main__":
+    preprocess_chen()
+    preprocess_zhu()
+    preprocess_roskams()
+    preprocess_ngo()
+    preprocess_ibarra()
+    preprocess_toden()
+    preprocess_chalasani()
+    preprocess_block()
+    preprocess_rozowsky()
+    preprocess_tao()
+    preprocess_wei()
+    preprocess_moufarrej()
+    preprocess_wang()
+    preprocess_giraldez()
+    preprocess_sun()
+    preprocess_decruyenaere()
+    preprocess_reggiardo()
