49876c3a6037d7a4fa095d9fb3da70217c329854
Marc Weber <marc.weber@flomics.com>
2025-05-15 12:03:04 +0200

    added mapping of SRA columns and values for disease

---
+++ sra_metadata/sra_columns_mapping.py
@@ -0,0 +1,27 @@
+mapping_disease = {
+    'Esophagus Cancer patient':'Esophagus cancer',
+    'Lung Cancer patient':'Lung cancer',
+    'Liver Cancer patient':'Liver cancer',
+    'Stomach Cancer patient':'Stomach cancer',
+    'Colorectal Cancer patient':'colorectal cancer',
+    'Healthy donor':'Control',
+    'NASH':'Nonalcoholic steatohepatitis',
+    'NAFLD':'Nonalcoholic fatty liver disease',
+    'Healthy':'Control',
+    'Control':'Control',
+    'Hepatoma'
+    'Cirrhosis':'Cirrhosis',
+    'pre-eclampsia':'Pre-eclampsia',
+    'severe pre-eclampsia':'Severe pre-eclampsia',
+    'liver cancer patient':'Liver cancer',
+    'healthy donor':'Control',
+    'normal':'Control',
+    'HCC':'Liver cancer',
+    'CHB':'Chronic hepatitis B',
+    'pancreatic cancer patient':'Pancreatic cancer',
+    'normal healthy donor':'Control'
+}
+# Rename column names:
+# disease -> disease
+# disease_state -> disease
+# subject_status -> disease
