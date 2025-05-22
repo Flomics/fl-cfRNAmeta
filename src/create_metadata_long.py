97f1536c249192dfbae9027979a4665fbb8f2694
Marc Weber <marc.weber@flomics.com>
2025-05-22 12:04:36 +0200

    refactor the merging of preprocessed metadata with dataset_metadata, part 1

--- src/create_metadata_long.py
+++ src/create_metadata_long.py
@@ -7,33 +7,8 @@ from sra_columns_mapping import rename_columns_and_values
 pd.set_option('display.max_columns', None)
 
 def main():
-    # Read plasma cfRNA-seq workflow comparison
-    csv_path = "../sra_metadata/dataset_metadata.tsv"
-    dataset_metadata = pd.read_csv(csv_path, sep = "\t")
 
-    chen = preprocess_chen()
-    zhu = preprocess_zhu()
-    roskams = preprocess_roskams()
-    ngo = preprocess_ngo()
-    ibarra = preprocess_ibarra()
-    toden = preprocess_toden()
-    chalasani = preprocess_chalasani()
-    block = preprocess_block()
-    rozowsky = preprocess_rozowsky()
-    tao = preprocess_tao()
-    wei = preprocess_wei()
-    moufarrej = preprocess_moufarrej()
-    wang = preprocess_wang()
-    giraldez = preprocess_giraldez()
-    sun = preprocess_sun()
-    decruyenaere = preprocess_decruyenaere()
-    reggiardo = preprocess_reggiardo()
 
-    chen = chen.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
-    zhu = zhu.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
-    roskams = roskams.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
-    ngo = ngo.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
-    ibarra = ibarra.merge(dataset_metadata[['dataset_short_name', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
     toden = toden.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
     chalasani = chalasani.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
     block = block.merge(dataset_metadata[['dataset_short_name','Biomaterial', 'Plasma tubes', 'DNAse','RNA extraction kit','RNA extraction kit (short name)','Library prep kit','Library prep kit (short name)']], on='dataset_short_name', how='left')
